1267ffe5070ca50a1ffda7051bdf8c02862719c1
Ali Taha Shakir <alitahashakir@gmail.com>
2020-09-09 15:06:45 +0500

    worked on firebase google auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import {signInWithGoogle} from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
 
@@ -37,7 +39,8 @@ class SignIn extends React.Component {
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email" required />
                     <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />
 
-                    <CustomButton>Sign In</CustomButton>
+                    <CustomButton type='submit'>Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} >Sign In with Google</CustomButton>
                 </form>
             </div>
         )
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyD3F_dg2uJpoDA250qdSY3Q3okBjBpBEW4",
+    authDomain: "crwn-db-ac6bf.firebaseapp.com",
+    databaseURL: "https://crwn-db-ac6bf.firebaseio.com",
+    projectId: "crwn-db-ac6bf",
+    storageBucket: "crwn-db-ac6bf.appspot.com",
+    messagingSenderId: "793669411702",
+    appId: "1:793669411702:web:411289bf30623d92871057",
+    measurementId: "G-XFCSMRYENX"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: 'select_account'
+});
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
